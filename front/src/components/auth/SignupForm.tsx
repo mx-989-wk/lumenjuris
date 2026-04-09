6b309575b9298489ce07474c63930dcc8d0dfdc3
julienb84 <julienbouchez@icloud.com>
2026-04-09 17:44:45 +0200

    création des éléments UI pour le signupForm et création du signupForm

--- front/src/components/auth/SignupForm.tsx
+++ front/src/components/auth/SignupForm.tsx
@@ -1,5 +1,203 @@
+// UI //
+import { Button } from "../ui/Button";
+import { Input } from "../ui/Input";
+import { InputGroup, InputGroupAddon, InputGroupInput } from "../ui/InputGroup";
+import {
+  Field,
+  FieldLabel,
+  FieldDescription,
+  FieldError,
+  FieldGroup,
+  FieldLegend,
+  FieldSeparator,
+  FieldSet,
+  FieldContent,
+  FieldTitle,
+} from "../ui/Field";
+import { Separator } from "../ui/Separator";
+import { Checkbox } from "../ui/Checkbox";
+import { Label } from "../ui/Label";
+import { EyeOffIcon } from "lucide-react";
+
+import { useState } from "react";
+
 const SignupForm = () => {
-  return <div></div>;
+  const [lastnameError, setLastnameError] = useState("");
+  const [firstnameError, setFirstnameError] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [submitLoading, setSubmitLoading] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+  const handleChangeLastname = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event);
+  };
+
+  const handleChangeFirstname = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    console.log(event);
+  };
+
+  const handleChangeEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event);
+  };
+
+  const handleChangePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event);
+  };
+
+  const handleChangeConfirmPassword = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    console.log(event);
+  };
+
+  const handleCheckNewsletter = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    const value = event.target.checked;
+    console.log(value);
+  };
+
+  return (
+    <div className="w-[420px] border border-border p-4 rounded-xl flex flex-col gap-4 bg-background">
+      <section>
+        <h2 className="font-semibold text-[18px]">
+          Créez un compte pour accéder à nos outils
+        </h2>
+        <p className="text-black/50">Complétez les champs suivants</p>
+      </section>
+      <form>
+        <div className="flex flex-col gap-6">
+          <div className="grid gap-2">
+            <Field>
+              <FieldLabel htmlFor="lastname">Nom</FieldLabel>
+              <Input
+                id="lastname"
+                type="text"
+                placeholder="Votre nom"
+                required
+                onChange={handleChangeLastname}
+              />
+            </Field>
+          </div>
+          <div className="grid gap-2">
+            <Field>
+              <FieldLabel htmlFor="firstname">Prénom</FieldLabel>
+              <Input
+                id="firstname"
+                type="text"
+                placeholder="Votre prénom"
+                required
+                onChange={handleChangeFirstname}
+              />
+            </Field>
+          </div>
+          <div className="grid gap-2">
+            <Field>
+              <FieldLabel htmlFor="email">Email</FieldLabel>
+              <Input
+                id="email"
+                type="email"
+                placeholder="Ex : mail@example.com"
+                required
+                onChange={handleChangeEmail}
+              />
+            </Field>
+          </div>
+          <div className="grid gap-2">
+            <Field className="max-w-sm">
+              <FieldLabel htmlFor="password">Password</FieldLabel>
+              <InputGroup>
+                <InputGroupInput
+                  id="password"
+                  type={showPassword ? "text" : "password"}
+                  placeholder="Choisissez un mot de passe"
+                  required
+                  onChange={handleChangePassword}
+                />
+                <InputGroupAddon align="inline-end">
+                  <EyeOffIcon />
+                </InputGroupAddon>
+              </InputGroup>
+              <FieldDescription>Au moins 8 charactères</FieldDescription>
+            </Field>
+          </div>
+          <div className="grid gap-2">
+            <Field className="max-w-sm">
+              <FieldLabel htmlFor="confirmpassword">
+                Confirm password
+              </FieldLabel>
+              <InputGroup>
+                <InputGroupInput
+                  id="confirmpassword"
+                  type={showConfirmPassword ? "text" : "password"}
+                  placeholder="Confirmez votre mot de passe"
+                  required
+                  onChange={handleChangeConfirmPassword}
+                />
+                <InputGroupAddon align="inline-end">
+                  <EyeOffIcon />
+                </InputGroupAddon>
+              </InputGroup>
+              <FieldDescription>
+                Saisissez à nouveau votre mot de passe
+              </FieldDescription>
+            </Field>
+          </div>
+          <div className="grid gap-2">
+            <Field>
+              <FieldLabel htmlFor="siren">Siren</FieldLabel>
+              <Input
+                id="siren"
+                type="text"
+                placeholder="Ex : 924242424"
+                required
+                onChange={handleChangeFirstname}
+              />
+              <FieldDescription>
+                Saisissez le numéro Siren de votre société
+              </FieldDescription>
+            </Field>
+          </div>
+          <div className="grid gap-2">
+            <FieldGroup className="mx-auto w-72">
+              <Field orientation="horizontal">
+                <Checkbox
+                  id="terms-checkbox-desc"
+                  name="terms-checkbox-desc"
+                  defaultChecked={false}
+                />
+                <FieldContent>
+                  <FieldLabel htmlFor="terms-checkbox-desc">
+                    Valider les CGU
+                  </FieldLabel>
+                  <FieldDescription>
+                    Vous confirmez accepter nos{" "}
+                    <a
+                      href="https://www.lumenjuris.com/conditions-generales-dutilisation/"
+                      className="hover:cursor-pointer"
+                    >
+                      CGU
+                    </a>
+                  </FieldDescription>
+                </FieldContent>
+              </Field>
+            </FieldGroup>
+          </div>
+          <div className="w-full h-px bg-border"></div>
+          <div className="grid gap-2">
+            <Button className="text-background">S'inscrire</Button>
+            <Button variant="ghost" className="border border-lumenjuris">
+              S'inscrire avec Google
+            </Button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default SignupForm;
--- front/src/page/Inscription.tsx
+++ front/src/page/Inscription.tsx
@@ -2,9 +2,14 @@ import SignupForm from "../components/auth/SignupForm";
 
 export function Inscription() {
   return (
-    <div>
-      <h1>Inscription</h1>
-      <SignupForm />
-    </div>
+    <>
+      <header className="h-16 border-b border-gray-200 bg-white flex items-center justify-between px-4 lg:px-6 sticky top-0 z-10"></header>
+      <div className="bg-lumenjuris-background min-h-[calc(100vh-64px)]">
+        <div className="w-[800px] mx-auto pt-10 flex flex-col items-center gap-5">
+          <h1>Créez un compte</h1>
+          <SignupForm />
+        </div>
+      </div>
+    </>
   );
 }
